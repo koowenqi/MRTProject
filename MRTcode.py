1d493b6763e0f16c0ab08c0c1c368959ffc89e08
koowenqi <wenqi2004@gmail.com>
2025-03-31 02:21:33 +0800

    added mrt lines, progress needs to enhance a bit, need to know more about project in depth

--- MRTcode.py
+++ MRTcode.py
@@ -1,11 +1,21 @@
 mrttest = []
+linesdict = {}
 
-mrtfile = open("MRT schedule data.txt", "a+")
+mrtfile = open("MRT schedule data.txt", "r")
 mrtfile.seek(0)
 for position,line in enumerate(mrtfile):
     if position <= 4:
         continue
     else:
-        mrttest.append(line)
+        mrttest.append(line.split(";"))
 
-print(mrttest)
+linesfile = open("MRT lines.txt", "r")
+linesfile.seek(0)
+for line in linesfile:
+    i = line.split(";")
+    linesdict[i[0]] = i[1]
+
+
+
+
+# print(mrttest)
